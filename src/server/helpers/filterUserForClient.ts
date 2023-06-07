dd90ba372f474d031774d718c4f03f41d307194b
Princelle Abbie Binobo <princellebinobo@gmail.com>
2023-06-07 16:51:55 +0400

    Profile page with SSG helper

---
+++ src/server/helpers/filterUserForClient.ts
@@ -0,0 +1,9 @@
+import { User } from "@clerk/backend/dist/types/api";
+
+export const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
